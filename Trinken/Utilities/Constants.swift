dc811b0b0f91beab96412f0deffebd8904048b26
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-06-03 16:13:19 -0300

    organize files in folders

---
+++ Trinken/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Trinken
+//
+//  Created by Gustavo Tiecker on 03/06/21.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let home = UIImage(systemName: "house")
+    static let search = UIImage(systemName: "magnifyingglass")
+    static let favorites = UIImage(systemName: "star")
+    static let configurations = UIImage(systemName: "gear")
+}

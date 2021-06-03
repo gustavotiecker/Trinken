0518875a64701552cb1e2703d95c3309eec074be
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-06-02 23:57:04 -0300

    present onboarding screen

---
+++ Trinken/View/Screens/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  Trinken
+//
+//  Created by Gustavo Tiecker on 01/06/21.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
--- Trinken/ViewController.swift
+++ Trinken/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if Core.shared.isNewUser() {
+            let welcomeViewController = WelcomeViewController()
+            welcomeViewController.modalPresentationStyle = .fullScreen
+            present(welcomeViewController, animated: true)
+        }
+    }
+}
 
-
+class Core {
+    
+    static let shared = Core()
+    
+    func isNewUser() -> Bool {
+        return !UserDefaults.standard.bool(forKey: "isNewUser")
+    }
+    
+    func setIsNotNewUser() {
+        UserDefaults.standard.set(true, forKey: "isNewUser")
+    }
 }
 

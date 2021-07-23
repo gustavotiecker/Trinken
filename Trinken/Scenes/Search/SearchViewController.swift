032b15499154b8319cfb6f73d4729a8a81ad3d63
Gustavo Tiecker <gustavotiecker@gmail.com>
2021-07-22 22:08:03 -0300

    change SearchViewController to work with UISearchBar

--- Trinken/Scenes/Search/SearchViewController.swift
+++ Trinken/Scenes/Search/SearchViewController.swift
@@ -10,53 +10,50 @@ import UIKit
 class SearchViewController: UIViewController {
     
     // MARK: - Properties
-    let searchTextField = BasicTextField(placeholder: "Search for a drink", icon: SFSymbols.SEARCH)
-    let searchButton = PrimaryButton(title: "Get Drinks", backgroundColor: Colors.SECONDARY_COLOR)
+    let searchBar = UISearchBar()
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubviews(searchTextField, searchButton)
-        navigationController?.isNavigationBarHidden = true
-        configureSearchTextField()
-        configureSearchButton()
-        createDismissKeyboardTapGesture()
-    }
-    
-    // MARK: - Actions
-    @objc private func searchButtonPressed() {
-        
+        configure()
     }
     
     // MARK: - Private Methods
+    private func configure() {
+        configureNavigation()
+        configureSearchBar()
+        createDismissKeyboardTapGesture()
+    }
+    
     private func createDismissKeyboardTapGesture() {
         let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
     
-    private func configureSearchTextField() {
-        searchTextField.delegate = self
-        
-        NSLayoutConstraint.activate([
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            searchTextField.heightAnchor.constraint(equalToConstant: 50)
-        ])
+    private func configureNavigation() {
+        navigationItem.titleView = searchBar
+        navigationController?.navigationBar.tintColor = Colors.PRIMARY_TINT_COLOR
     }
     
-    private func configureSearchButton() {
-        searchButton.addTarget(self, action: #selector(searchButtonPressed), for: .touchUpInside)
+    private func configureSearchBar() {
+        searchBar.showsCancelButton = false
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+        searchBar.becomeFirstResponder()
+    }
+    
+    private func search(shouldShow: Bool) {
         
-        NSLayoutConstraint.activate([
-            searchButton.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 10),
-            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            searchButton.heightAnchor.constraint(equalToConstant: 50)
-        ])
     }
 }
 
-extension SearchViewController: UITextFieldDelegate {
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.resignFirstResponder()
+    }
     
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
 }
